cbe36c284c20ad3b32a8b5c6f4b99066e39ae362
Mihael Isaev <isaev.mihael@gmail.com>
2019-06-13 16:16:41 +0400

    Deprecate `desiredStatusCode` because of `successStatusCodes`

--- CodyFire/Classes/APIRequest.swift
+++ CodyFire/Classes/APIRequest.swift
@@ -42,7 +42,7 @@ public class APIRequest<ResultType: Decodable> {
     var payload: PayloadProtocol?
     var query: String?
     var headers: [String: String] = CodyFire.shared.globalHeaders
-    var desiredStatusCode: StatusCode = .ok
+    var successStatusCodes: [StatusCode] = [.ok]
     var successCallback: SuccessResponse?
     var errorCallback: ErrorResponse?
     var notAuthorizedCallback: NotAuthorizedResponse?
